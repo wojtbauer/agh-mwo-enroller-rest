3aeb3e76a97d4372c8919ded0962f811a0a4ae66
student15 <student15@pcoip.ki.agh.edu.pl>
2019-04-14 14:46:06 +0200

    x

--- src/main/java/com/company/enroller/controllers/ParticipantRestController.java
+++ src/main/java/com/company/enroller/controllers/ParticipantRestController.java
@@ -5,6 +5,8 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,5 +26,42 @@ public class ParticipantRestController {
 		Collection<Participant> participants = participantService.getAll();
 		return new ResponseEntity<Collection<Participant>>(participants, HttpStatus.OK);
 	}
-
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<?> getMeeting(@PathVariable("id") String login) {
+	     Participant participant = participantService.findByLogin(login);
+	     if (participant == null) {
+	         return new ResponseEntity(HttpStatus.NOT_FOUND);
+	     }
+	     return new ResponseEntity<Participant>(participant, HttpStatus.OK);
+	 }
+	
+	 @RequestMapping(value = "", method = RequestMethod.POST)
+	 public ResponseEntity<?> registerParticipant(@RequestBody Participant participant){
+		 Participant foundParticipant = participantService.findByLogin(participant.getLogin());
+	     if (foundParticipant == null) {
+	    	 return new ResponseEntity("Unable to create. A participant with login " + participant.getLogin() + " already exist.", HttpStatus.CONFLICT);
+	     }
+	     participantService.add(participant);
+	     return new ResponseEntity<Participant>(participant,HttpStatus.CREATED);
+	 }
+	 
+		@RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+		public ResponseEntity<?> deleteParticipants(@PathVariable("id") String login) {
+			Participant participant = participantService.findByLogin(login);
+			if (participant == null) {
+		         return new ResponseEntity(HttpStatus.NOT_FOUND);   
+		     }
+			participantService.delete(participant);
+		     return new ResponseEntity<Participant>(participant,HttpStatus.OK);
+		}
+		
+		@RequestMapping(value = "{id}", method = RequestMethod.PUT)
+		public ResponseEntity<?> editParticipants(@PathVariable("id") String login) {
+			Participant participant = participantService.findByLogin(login);
+			if (participant == null) {
+		         return new ResponseEntity(HttpStatus.NOT_FOUND);   
+		     }
+			participantService.edit(participant);
+		     return new ResponseEntity<Participant>(participant,HttpStatus.OK);
+		}
 }
--- src/main/java/com/company/enroller/persistence/ParticipantService.java
+++ src/main/java/com/company/enroller/persistence/ParticipantService.java
@@ -2,6 +2,7 @@ package com.company.enroller.persistence;
 
 import java.util.Collection;
 
+import org.hibernate.Transaction;
 import org.springframework.stereotype.Component;
 
 import com.company.enroller.model.Participant;
@@ -17,6 +18,30 @@ public class ParticipantService {
 
 	public Collection<Participant> getAll() {
 		return connector.getSession().createCriteria(Participant.class).list();
+	}
+
+	public Participant findByLogin(String login) {
+		return (Participant) connector.getSession().get(Participant.class, login);
+	}
+
+	public void add(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().save(participant);
+		transaction.commit();
+		
+	}
+	public void delete(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().delete(participant);
+		transaction.commit();
+		
+	}
+	
+	public void edit(Participant participant) {
+		Transaction transaction = connector.getSession().beginTransaction();
+		connector.getSession().edit(participant);
+		transaction.commit();
+		
 	}
 
 }
